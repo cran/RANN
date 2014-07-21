602d01f6696a045f196c5df8f1b81ff648d6047c
Gregory Jefferis <jefferis@gmail.com>
2014-07-21 00:00:00 +0000

    version 2.4.1

--- src/kd_dump.cpp
+++ src/kd_dump.cpp
@@ -41,7 +41,7 @@ using namespace std;					// make std:: available
 //----------------------------------------------------------------------
 
 const int		STRING_LEN		= 500;	// maximum string length
-const double	EPSILON			= 1E-5; // small number for float comparison
+//const double	EPSILON			= 1E-5; // small number for float comparison
 
 enum ANNtreeType {KD_TREE, BD_TREE};	// tree types (used in loading)
 
@@ -439,6 +439,7 @@ static ANNkd_ptr annReadTree(
 	}
 	else {
 		annError("Illegal node type in dump file", ANNabort);
+    return NULL;            // to keep the compiler happy
 #ifndef RANN
 		exit(0);								// to keep the compiler happy
 #endif
